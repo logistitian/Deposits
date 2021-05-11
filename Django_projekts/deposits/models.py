75e65c58657ef777b01ca2d289566a26fbdde131
logistitian <lauriskravalis@gmail.com>
2021-05-11 23:28:32 +0300

    Start to provide interest - but not sure that correct!

--- Django_projekts/deposits/models.py
+++ Django_projekts/deposits/models.py
@@ -5,4 +5,7 @@ class Deposit(models.Model):
     deposit = models.IntegerField()
     term = models.IntegerField()
     rate = models.DecimalField(decimal_places=2, max_digits=4)
-    interest = models.IntegerField()
+
+    def interest(self):
+        interest = int(self.deposit * ((1 + self.rate)*self.term))
+        return interest
