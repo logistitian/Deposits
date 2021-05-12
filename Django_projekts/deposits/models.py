a1276d1787da91db1648f97024df4849212dbef2
logistitian <lauriskravalis@gmail.com>
2021-05-12 07:14:37 +0300

    Pievienoju "DetailView" - DONE! Izlaboju"Interest rate formulu" - DONE!

--- Django_projekts/deposits/models.py
+++ Django_projekts/deposits/models.py
@@ -7,5 +7,5 @@ class Deposit(models.Model):
     rate = models.DecimalField(decimal_places=2, max_digits=4)
 
     def interest(self):
-        interest = int(self.deposit * ((1 * self.rate)*self.term))
+        interest = float((self.deposit * ((1 + self.rate)**self.term))-self.deposit)
         return interest
